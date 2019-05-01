2eb16613a50141db80e5d9e62fdfe2cfa3919355
wangxu <593464677@qq.com>
2019-05-02 01:20:32 +0800

    新增index接口修改位置

--- src/main/java/com/example/jekins/controller/HelloController.java
+++ src/main/java/com/example/jekins/controller/HelloController.java
@@ -1,4 +1,12 @@
 package com.example.jekins.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+    @GetMapping("/index")
+    public String htllo(){
+        return "Welcome To Index Page !";
+    }
 }
